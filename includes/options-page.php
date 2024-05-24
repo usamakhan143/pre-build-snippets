3fc9e22af85f64434cef9b8bf66358b07f533b6d
Usama Khan <usamaoff796@gmail.com>
2024-05-25 04:20:21 +0500

    New feature added for one click tm extra product option plugin single product template css.

--- includes/options-page.php
+++ includes/options-page.php
@@ -3,116 +3,123 @@
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-add_action( 'carbon_fields_register_fields', 'create_options_page_pbs' );
-function create_options_page_pbs() {
-    Container::make( 'theme_options', __( 'Pre Build Snippets' ) )
-    ->set_icon('dashicons-media-code')
-    ->add_fields([
-        Field::make( 'checkbox', 'add_to_cart_button_text_active', __( 'Change WooCommerce Add to cart button text' ) ),
-        Field::make( 'text', 'add_to_cart_button_text', 'Add to Cart Button Text' )
-        ->set_conditional_logic( array(
-                array(
-                    'field' => 'add_to_cart_button_text_active',
-                    'value' => true,
-                )
-        ))
-        ->set_attribute('placeholder', 'Enter Custom Text for Add to cart Button')
-    
-    ])
-    ->add_fields([
-        Field::make( 'checkbox', 'cart_remove_active', __( 'Delete WooCommerce cart items on the checkout page' ) ),
-        // ->set_html( '<h2>Lorem ipsum</h2><p>Quisque mattis ligula.</p>' ),\\
-        Field::make( 'html', 'crb_information_text' )
-    ->set_html( '<img src="' . PRE_BUILD_SNIPPETS_URL . 'assets/images/cart-remove.png" width="300"/>')
-    ])
-    ->add_fields([
-        Field::make( 'checkbox', 'cart_empty_leaving_checkout_active', __( 'Empty the WooCommerce cart after leaving the checkout without making a purchase' ) ),
-    ])
-    ->add_fields([
-        Field::make( 'checkbox', 'return_to_shop_url_active', __( 'Change WooCommerce "Return to Shop" URL' ) ),
-        Field::make( 'text', 'return_to_shop_url', 'Change Return to Shop URL' )
-        ->set_conditional_logic( array(
-                array(
-                    'field' => 'return_to_shop_url_active',
-                    'value' => true,
-                )
-        ))
-        ->set_attribute('placeholder', 'Enter Custom URL')
-        ->set_help_text('https://domain.com'),
-        // ->set_html( '<h2>Lorem ipsum</h2><p>Quisque mattis ligula.</p>' ),\\
-        Field::make( 'html', 'return_to_shop_url_information_text' )
-    ->set_html( '<img src="' . PRE_BUILD_SNIPPETS_URL . 'assets/images/return-to-shop.png" width="300"/>')
-    ])
-    ->add_fields([
-        Field::make( 'checkbox', 'return_to_shop_text_active', __( 'Change WooCommerce "Return to Shop" Text' ) ),
-        Field::make( 'text', 'return_to_shop_text', 'Change Return to Shop Button Text' )
-        ->set_conditional_logic( array(
-                array(
-                    'field' => 'return_to_shop_text_active',
-                    'value' => true,
-                )
-        ))
-        ->set_attribute('placeholder', 'Enter Custom Text')
-        ->set_help_text('E.g. Back to Home'),
-        // ->set_html( '<h2>Lorem ipsum</h2><p>Quisque mattis ligula.</p>' ),\\
-        Field::make( 'html', 'return_to_shop_text_information_text' )
-    ->set_html( '<img src="' . PRE_BUILD_SNIPPETS_URL . 'assets/images/return-to-shop-text.png" width="300"/>')
-    ])
-    ->add_fields([
-        Field::make( 'checkbox', 'wc_price_suffix_active', __( 'Add Suffix to WooCommerce Price' ) ),
-        Field::make( 'text', 'wc_suffix_price', 'Suffix' )
-        ->set_conditional_logic( array(
-                array(
-                    'field' => 'wc_price_suffix_active',
-                    'value' => true,
-                )
-        ))
-        ->set_attribute('placeholder', 'Enter Suffix Text')
-        ->set_help_text('E.g. $20 per day, The "per day" is a suffix')
-    ])
-    ->add_fields([
-        Field::make( 'checkbox', 'wc_price_prefix_active', __( 'Add Prefix to WooCommerce Price' ) ),
-        Field::make( 'text', 'wc_prefix_price', 'Prefix' )
-        ->set_conditional_logic( array(
-                array(
-                    'field' => 'wc_price_prefix_active',
-                    'value' => true,
-                )
-        ))
-        ->set_attribute('placeholder', 'Enter Prefix Text')
-        ->set_help_text('E.g. Starting from $20, The "Starting from" is a Prefix')
-    ])
-    ->add_fields([
-        Field::make( 'checkbox', 'custom_add_to_cart_btn_active', __( 'Create a custom button on the WooCommerce catalog page that can navigate you to the single product page' ) ),
-        Field::make( 'text', 'custom_add_to_cart_btn', 'Button Text' )
-        ->set_conditional_logic( array(
-                array(
-                    'field' => 'custom_add_to_cart_btn_active',
-                    'value' => true,
-                )
-        ))
-        ->set_attribute('placeholder', 'Enter Custom Text')
-    ])
-    ->add_fields([
-        Field::make( 'checkbox', 'login_before_checkout_active', __( 'Login before WooCommerce checkout' ) ),
-        Field::make( 'text', 'wc_checkout_page_id', 'WooCommerce Checkout Page ID' )
-        ->set_conditional_logic( array(
-                array(
-                    'field' => 'login_before_checkout_active',
-                    'value' => true,
-                )
-        ))
-        ->set_attribute('placeholder', 'Enter your WooCommerce Checkout Page ID')
-    ])
-    ->add_fields([
-        Field::make( 'checkbox', 'yith_booking_checkout_css_active', __( 'Checkout CSS when Yith Booking Enabled' ) ),
-        Field::make( 'html', 'yith_booking_checkout_css_information_text' )
-    ->set_html( '<img src="' . PRE_BUILD_SNIPPETS_URL . 'assets/images/yith-booking-checkout-css.png" width="300"/>')
-        // ->set_help_text('If Yith Booking is enable use this css to fix the checkout order details section')
-    ]);  
+add_action('carbon_fields_register_fields', 'create_options_page_pbs');
+function create_options_page_pbs()
+{
+    Container::make('theme_options', __('Pre Build Snippets'))
+        ->set_icon('dashicons-media-code')
+        ->add_fields([
+            Field::make('checkbox', 'add_to_cart_button_text_active', __('Change WooCommerce Add to cart button text')),
+            Field::make('text', 'add_to_cart_button_text', 'Add to Cart Button Text')
+                ->set_conditional_logic(array(
+                    array(
+                        'field' => 'add_to_cart_button_text_active',
+                        'value' => true,
+                    )
+                ))
+                ->set_attribute('placeholder', 'Enter Custom Text for Add to cart Button')
+
+        ])
+        ->add_fields([
+            Field::make('checkbox', 'cart_remove_active', __('Delete WooCommerce cart items on the checkout page')),
+            // ->set_html( '<h2>Lorem ipsum</h2><p>Quisque mattis ligula.</p>' ),\\
+            Field::make('html', 'crb_information_text')
+                ->set_html('<img src="' . PRE_BUILD_SNIPPETS_URL . 'assets/images/cart-remove.png" width="300"/>')
+        ])
+        ->add_fields([
+            Field::make('checkbox', 'cart_empty_leaving_checkout_active', __('Empty the WooCommerce cart after leaving the checkout without making a purchase')),
+        ])
+        ->add_fields([
+            Field::make('checkbox', 'return_to_shop_url_active', __('Change WooCommerce "Return to Shop" URL')),
+            Field::make('text', 'return_to_shop_url', 'Change Return to Shop URL')
+                ->set_conditional_logic(array(
+                    array(
+                        'field' => 'return_to_shop_url_active',
+                        'value' => true,
+                    )
+                ))
+                ->set_attribute('placeholder', 'Enter Custom URL')
+                ->set_help_text('https://domain.com'),
+            // ->set_html( '<h2>Lorem ipsum</h2><p>Quisque mattis ligula.</p>' ),\\
+            Field::make('html', 'return_to_shop_url_information_text')
+                ->set_html('<img src="' . PRE_BUILD_SNIPPETS_URL . 'assets/images/return-to-shop.png" width="300"/>')
+        ])
+        ->add_fields([
+            Field::make('checkbox', 'return_to_shop_text_active', __('Change WooCommerce "Return to Shop" Text')),
+            Field::make('text', 'return_to_shop_text', 'Change Return to Shop Button Text')
+                ->set_conditional_logic(array(
+                    array(
+                        'field' => 'return_to_shop_text_active',
+                        'value' => true,
+                    )
+                ))
+                ->set_attribute('placeholder', 'Enter Custom Text')
+                ->set_help_text('E.g. Back to Home'),
+            // ->set_html( '<h2>Lorem ipsum</h2><p>Quisque mattis ligula.</p>' ),\\
+            Field::make('html', 'return_to_shop_text_information_text')
+                ->set_html('<img src="' . PRE_BUILD_SNIPPETS_URL . 'assets/images/return-to-shop-text.png" width="300"/>')
+        ])
+        ->add_fields([
+            Field::make('checkbox', 'wc_price_suffix_active', __('Add Suffix to WooCommerce Price')),
+            Field::make('text', 'wc_suffix_price', 'Suffix')
+                ->set_conditional_logic(array(
+                    array(
+                        'field' => 'wc_price_suffix_active',
+                        'value' => true,
+                    )
+                ))
+                ->set_attribute('placeholder', 'Enter Suffix Text')
+                ->set_help_text('E.g. $20 per day, The "per day" is a suffix')
+        ])
+        ->add_fields([
+            Field::make('checkbox', 'wc_price_prefix_active', __('Add Prefix to WooCommerce Price')),
+            Field::make('text', 'wc_prefix_price', 'Prefix')
+                ->set_conditional_logic(array(
+                    array(
+                        'field' => 'wc_price_prefix_active',
+                        'value' => true,
+                    )
+                ))
+                ->set_attribute('placeholder', 'Enter Prefix Text')
+                ->set_help_text('E.g. Starting from $20, The "Starting from" is a Prefix')
+        ])
+        ->add_fields([
+            Field::make('checkbox', 'custom_add_to_cart_btn_active', __('Create a custom button on the WooCommerce catalog page that can navigate you to the single product page')),
+            Field::make('text', 'custom_add_to_cart_btn', 'Button Text')
+                ->set_conditional_logic(array(
+                    array(
+                        'field' => 'custom_add_to_cart_btn_active',
+                        'value' => true,
+                    )
+                ))
+                ->set_attribute('placeholder', 'Enter Custom Text')
+        ])
+        ->add_fields([
+            Field::make('checkbox', 'login_before_checkout_active', __('Login before WooCommerce checkout')),
+            Field::make('text', 'wc_checkout_page_id', 'WooCommerce Checkout Page ID')
+                ->set_conditional_logic(array(
+                    array(
+                        'field' => 'login_before_checkout_active',
+                        'value' => true,
+                    )
+                ))
+                ->set_attribute('placeholder', 'Enter your WooCommerce Checkout Page ID')
+        ])
+        ->add_fields([
+            Field::make('checkbox', 'yith_booking_checkout_css_active', __('Checkout CSS when Yith Booking Enabled')),
+            Field::make('html', 'yith_booking_checkout_css_information_text')
+                ->set_html('<img src="' . PRE_BUILD_SNIPPETS_URL . 'assets/images/yith-booking-checkout-css.png" width="300"/>')
+            // ->set_help_text('If Yith Booking is enable use this css to fix the checkout order details section')
+        ])
+        ->add_fields([
+            Field::make('checkbox', 'tm_extraprooption_css_active', __('TM Extra Product Option CSS on single product page')),
+            Field::make('html', 'tm_extraprooption_css_information_text')
+                ->set_html('<img src="' . PRE_BUILD_SNIPPETS_URL . 'assets/images/tm-extraprooption-css.png" width="300"/>')
+        ]);
 }
 
-add_action( 'after_setup_theme', 'crb_load_pbs' );
-function crb_load_pbs() {
+add_action('after_setup_theme', 'crb_load_pbs');
+function crb_load_pbs()
+{
     \Carbon_Fields\Carbon_Fields::boot();
 }
--- includes/pre-build-snippets.php
+++ includes/pre-build-snippets.php
@@ -1,65 +1,66 @@
 <?php
 
 add_action('wp', 'run_filter');
-function run_filter() {
+function run_filter()
+{
 
-    if(get_pbs_fields('add_to_cart_button_text_active') == 'yes') {
-        add_filter( 'woocommerce_product_add_to_cart_text', 'add_to_cart_button_text' );
-        add_filter( 'woocommerce_product_single_add_to_cart_text', 'add_to_cart_button_text' );
-    }
-
-    if(get_pbs_fields('cart_remove_active') == 'yes') {
-        add_filter( 'woocommerce_cart_item_name', 'delete_cart_items_from_checkout_page', 10, 3 );
+    if (get_pbs_fields('add_to_cart_button_text_active') == 'yes') {
+        add_filter('woocommerce_product_add_to_cart_text', 'add_to_cart_button_text');
+        add_filter('woocommerce_product_single_add_to_cart_text', 'add_to_cart_button_text');
     }
 
-    if(get_pbs_fields('cart_empty_leaving_checkout_active') == 'yes') {
-        add_action( 'wp_head', 'bryce_clear_cart' );
+    if (get_pbs_fields('cart_remove_active') == 'yes') {
+        add_filter('woocommerce_cart_item_name', 'delete_cart_items_from_checkout_page', 10, 3);
     }
 
-    if(get_pbs_fields('return_to_shop_url_active') == 'yes') {
-        add_filter( 'woocommerce_return_to_shop_redirect', 'pbs_wc_empty_cart_redirect_url' );
+    if (get_pbs_fields('cart_empty_leaving_checkout_active') == 'yes') {
+        add_action('wp_head', 'bryce_clear_cart');
     }
 
-    if(get_pbs_fields('return_to_shop_text_active') == 'yes') {
-        add_filter( 'gettext', 'change_wc_return_to_shop_text', 20, 3 );
+    if (get_pbs_fields('return_to_shop_url_active') == 'yes') {
+        add_filter('woocommerce_return_to_shop_redirect', 'pbs_wc_empty_cart_redirect_url');
     }
 
-    if(get_pbs_fields('wc_price_suffix_active') == 'yes') {
-        add_filter( 'woocommerce_get_price_html', 'pbs_add_price_suffix' );
-		add_filter( 'woocommerce_cart_item_price', 'pbs_add_price_suffix' );
+    if (get_pbs_fields('return_to_shop_text_active') == 'yes') {
+        add_filter('gettext', 'change_wc_return_to_shop_text', 20, 3);
     }
 
-    if(get_pbs_fields('wc_price_prefix_active') == 'yes') {
-        add_filter( 'woocommerce_get_price_html', 'pbs_add_price_prefix', 99, 2 );
+    if (get_pbs_fields('wc_price_suffix_active') == 'yes') {
+        add_filter('woocommerce_get_price_html', 'pbs_add_price_suffix');
+        add_filter('woocommerce_cart_item_price', 'pbs_add_price_suffix');
     }
 
-    if(get_pbs_fields('custom_add_to_cart_btn_active') == 'yes') {
-        add_filter( 'woocommerce_loop_add_to_cart_link', 'pbs_custom_view_product_button', 10, 2 );
-    }
-    
-    if(get_pbs_fields('login_before_checkout_active') == 'yes') {
-        add_filter( 'template_redirect', 'check_if_logged_in_before_wc_checkout', 10, 2 );
-    }
-    
-    if(get_pbs_fields('yith_booking_checkout_css_active') == 'yes') {
-        add_action( 'wp_enqueue_scripts', 'pbs_enqueue_custom_css' );
+    if (get_pbs_fields('wc_price_prefix_active') == 'yes') {
+        add_filter('woocommerce_get_price_html', 'pbs_add_price_prefix', 99, 2);
     }
 
+    if (get_pbs_fields('custom_add_to_cart_btn_active') == 'yes') {
+        add_filter('woocommerce_loop_add_to_cart_link', 'pbs_custom_view_product_button', 10, 2);
+    }
+
+    if (get_pbs_fields('login_before_checkout_active') == 'yes') {
+        add_filter('template_redirect', 'check_if_logged_in_before_wc_checkout', 10, 2);
+    }
+
+    if (get_pbs_fields('yith_booking_checkout_css_active') == 'yes') {
+        add_action('wp_enqueue_scripts', 'pbs_enqueue_custom_css');
+    }
+
+    if (get_pbs_fields('tm_extraprooption_css_active') == 'yes') {
+        add_action('wp_enqueue_scripts', 'pbs_enqueue_extraprocss');
+    }
 }
 
 function add_to_cart_button_text()
 {
-    if(get_pbs_fields('add_to_cart_button_text_active') == 'yes') {
-        if(in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) )) {
+    if (get_pbs_fields('add_to_cart_button_text_active') == 'yes') {
+        if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
             $add_to_cart_text = get_pbs_fields('add_to_cart_button_text');
-            return __( $add_to_cart_text, 'woocommerce' );
-        }
-        
-        else {
+            return __($add_to_cart_text, 'woocommerce');
+        } else {
             return 'WooCommerce plugin should be installed before use this feature.';
         }
-    }
-    else {
+    } else {
         return __('Add to cart', 'woocommerce');
     }
 }
@@ -73,20 +74,21 @@ function add_to_cart_button_text()
  * @param string $cart_item_key 
  * @return string
  */
-function delete_cart_items_from_checkout_page($product_name, $cart_item, $cart_item_key) {
-    
-    if(get_pbs_fields('cart_remove_active') == 'yes') {
-        if ( is_checkout() ) {
-            $_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
-            $product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
+function delete_cart_items_from_checkout_page($product_name, $cart_item, $cart_item_key)
+{
 
-            $remove_link = apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
+    if (get_pbs_fields('cart_remove_active') == 'yes') {
+        if (is_checkout()) {
+            $_product = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
+            $product_id = apply_filters('woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key);
+
+            $remove_link = apply_filters('woocommerce_cart_item_remove_link', sprintf(
                 '<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">×</a>',
-                esc_url( WC()->cart->get_remove_url( $cart_item_key ) ),
-                __( 'Remove this item', 'woocommerce' ),
-                esc_attr( $product_id ),
-                esc_attr( $_product->get_sku() )
-            ), $cart_item_key );
+                esc_url(WC()->cart->get_remove_url($cart_item_key)),
+                __('Remove this item', 'woocommerce'),
+                esc_attr($product_id),
+                esc_attr($_product->get_sku())
+            ), $cart_item_key);
 
             return '<span>' . $remove_link . '</span> <span>' . $product_name . '</span>';
         }
@@ -95,89 +97,89 @@ function delete_cart_items_from_checkout_page($product_name, $cart_item, $cart_i
 }
 
 
-function bryce_clear_cart() {
+function bryce_clear_cart()
+{
 
-    if(get_pbs_fields('cart_empty_leaving_checkout_active') == 'yes') {
-        if ( wc_get_page_id( 'cart' ) == get_the_ID() || wc_get_page_id( 'checkout' ) == get_the_ID() ) {
+    if (get_pbs_fields('cart_empty_leaving_checkout_active') == 'yes') {
+        if (wc_get_page_id('cart') == get_the_ID() || wc_get_page_id('checkout') == get_the_ID()) {
             return;
         }
-        WC()->cart->empty_cart( true );
+        WC()->cart->empty_cart(true);
     }
-
 }
 
 
-function pbs_wc_empty_cart_redirect_url() {
+function pbs_wc_empty_cart_redirect_url()
+{
 
-    if(get_pbs_fields('return_to_shop_url_active') == 'yes') {
+    if (get_pbs_fields('return_to_shop_url_active') == 'yes') {
         $url = get_pbs_fields('return_to_shop_url'); // change this link to your need
-        return esc_url( $url );
+        return esc_url($url);
     }
-
 }
 
 
-function change_wc_return_to_shop_text( $translated_text, $text, $domain ) {
+function change_wc_return_to_shop_text($translated_text, $text, $domain)
+{
 
-    if(get_pbs_fields('return_to_shop_text_active') == 'yes') {
+    if (get_pbs_fields('return_to_shop_text_active') == 'yes') {
         $custom_text_rts = get_pbs_fields('return_to_shop_text');
-        switch ( $translated_text ) {
-            case 'Return to shop' :
-                $translated_text = __( $custom_text_rts, 'woocommerce' );
+        switch ($translated_text) {
+            case 'Return to shop':
+                $translated_text = __($custom_text_rts, 'woocommerce');
                 break;
         }
         return $translated_text;
     }
-
 }
 
-  
-function pbs_add_price_suffix( $price ){
-    
-    if(get_pbs_fields('wc_price_suffix_active') == 'yes') {
+
+function pbs_add_price_suffix($price)
+{
+
+    if (get_pbs_fields('wc_price_suffix_active') == 'yes') {
         $wc_suffix = get_pbs_fields('wc_suffix_price');
-        $price .= ' '.$wc_suffix;
+        $price .= ' ' . $wc_suffix;
         return $price;
     }
-
 }
 
 
-function pbs_add_price_prefix( $price, $product ){
-    
-    if(get_pbs_fields('wc_price_prefix_active') == 'yes') {
+function pbs_add_price_prefix($price, $product)
+{
+
+    if (get_pbs_fields('wc_price_prefix_active') == 'yes') {
         $wc_prefix = get_pbs_fields('wc_prefix_price');
-        $price = $wc_prefix.' '. $price;
+        $price = $wc_prefix . ' ' . $price;
         return $price;
     }
 }
 
 
 /**
-* @snippet Change WooCommerce ‘Add to Cart’ button to 'Custom Button'
-* @compatible WC 4.3.1
-*/
+ * @snippet Change WooCommerce ‘Add to Cart’ button to 'Custom Button'
+ * @compatible WC 4.3.1
+ */
 // Change WooCommerce 'Add To Cart' button to 'View Product'
-function pbs_custom_view_product_button( $button, $product  ) {
-        
-    if(get_pbs_fields('custom_add_to_cart_btn_active') == 'yes') {
+function pbs_custom_view_product_button($button, $product)
+{
+
+    if (get_pbs_fields('custom_add_to_cart_btn_active') == 'yes') {
         // Ignore for variable products
-        if( $product->is_type( 'variable' ) ) return $button;
+        if ($product->is_type('variable')) return $button;
         // Button text here
         $btn_text = get_pbs_fields('custom_add_to_cart_btn');
-        $button_text = __( $btn_text, "woocommerce" );
-            return '<a class="button pbs-custom-view-product-button" href="' . $product->get_permalink() . '">' . $button_text . '</a>';
+        $button_text = __($btn_text, "woocommerce");
+        return '<a class="button pbs-custom-view-product-button" href="' . $product->get_permalink() . '">' . $button_text . '</a>';
     }
-
 }
 
 
 function check_if_logged_in_before_wc_checkout()
 {
-    if(get_pbs_fields('login_before_checkout_active') == 'yes') {
+    if (get_pbs_fields('login_before_checkout_active') == 'yes') {
         $pageid = get_pbs_fields('wc_checkout_page_id'); // your checkout page id
-        if(!is_user_logged_in() && is_page($pageid))
-        {
+        if (!is_user_logged_in() && is_page($pageid)) {
             $url = add_query_arg(
                 'redirect_to',
                 get_permalink($pagid),
@@ -190,9 +192,17 @@ function check_if_logged_in_before_wc_checkout()
 }
 
 
-function pbs_enqueue_custom_css(){
-    
-    if(get_pbs_fields('yith_booking_checkout_css_active') == 'yes') {
+function pbs_enqueue_custom_css()
+{
+
+    if (get_pbs_fields('yith_booking_checkout_css_active') == 'yes') {
         wp_enqueue_style('pre-build-snippets', PRE_BUILD_SNIPPETS_URL . 'assets/css/pbs.css');
+    }
+}
+
+function pbs_enqueue_extraprocss()
+{
+    if (get_pbs_fields('tm_extraprooption_css_active') == 'yes') {
+        wp_enqueue_style('pre-build-snippets-tm-extra', PRE_BUILD_SNIPPETS_URL . 'assets/css/tm-extra-product.css');
     }
 }
